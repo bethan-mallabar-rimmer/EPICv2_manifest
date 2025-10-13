a7b7544f77fbc6a161b0c87625478eb444a38fd5
Bethan <148891983+bethan-mallabar-rimmer@users.noreply.github.com>
2025-10-13 13:05:53 +0100

    Update functions.R
    add option for distance based promoter/enhancer annotation

--- functions.R
+++ functions.R
@@ -11,6 +11,10 @@ expand_annotation <- function(manifest, by='gene', verbose=TRUE) {
       print('Returning CpGs with annotated GENCODEv47 transcripts.')
       print('Important! This function assumes your CpG IDs/IlmnIDs are in the first column of input file, and the input file contains columns named GENCODEv47_Transcript_ID and/or GENCODEv47_Transcript_Name.')
     }
+    if (by=='db') {
+      print('Returning CpGs with annotated regulatory elements (based on distance from genes).')
+      print('Important! This function assumes your CpG IDs/IlmnIDs are in the first column of input file, and the input file contains columns named DB_Element_Gene_ID and/or DB_Element_Gene_Name')
+    }
     print('Set verbose = FALSE to silence this message.')
   }
 
@@ -78,4 +82,43 @@ expand_annotation <- function(manifest, by='gene', verbose=TRUE) {
     mtrans <- mtrans[,which(colnames(mtrans) != 'temp')]
     return(mtrans)
   }
+  if (by=='db') {
+    db_cols <- colnames(manifest)[grepl('DB_Element',colnames(manifest))]
+    gname <- 'DB_Element_Gene_Name' %in% db_cols
+    gid  <- 'DB_Element_Gene_ID' %in% db_cols
+    etype  <- 'DB_Element_Type' %in% db_cols
+
+  
+    if (!gname & !gid) {
+      stop('Columns DB_Element_Gene_Name and DB_Element_Gene_ID not found in input file.')
+    }
+  
+    temp <- tidyr::separate_longer_delim(manifest,
+                                       cols=all_of(db_cols),
+                                       delim=';')
+  
+    if (gid) {
+      temp$temp <- paste0(temp[,1], temp$DB_Element_Gene_ID)
+    } else {
+      temp$temp <- paste0(temp[,1], temp$DB_Element_Gene_Name)
+    }
+  
+    temp <- temp[!duplicated(temp$temp),]
+  
+    if (gname) {
+      mgenes <- temp[,colnames(temp) %in% c(colnames(temp)[1], 'DB_Element_Gene_Name', 'temp')]
+    } else {
+      mgenes <- temp[,colnames(temp) %in% c(colnames(temp)[1], 'DB_Element_Gene_ID', 'temp')]
+    }
+    if (etype) {
+      mgenes$Element <- rep('',nrow(mgenes))
+      mgenes$Element[grepl('Promoter',temp$DB_Element_Type,ignore.case = TRUE)] <- 'Promoter'
+      mgenes$Element[grepl('Enhancer',temp$DB_Element_Type,ignore.case = TRUE)] <- 'Enhancer'
+    }
+    colnames(mgenes)[2] <- 'Gene'
+    mgenes <- left_join(mgenes, temp[,-1], by='temp')
+    mgenes <- mgenes[,which(colnames(mgenes) != 'temp')]
+    return(mgenes)
+  }
+  
 }
